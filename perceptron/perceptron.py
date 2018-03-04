cbc4c72a2b2abc0d3eec2d0f457154121facd739
ying dai <dai92817@icloud.com>
2018-03-04 17:12:42 +0800

    Auto commit

---
+++ perceptron/perceptron.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.array([
+    [3, 3],
+    [4, 3],
+    [1, 1]
+])
+
+y = np.array([1, 1, -1])
+
+for i in range(len(y)):
+    if y[i] == 1:
+        plt.plot(X[i][0], X[i][1], 'bo')
+    else:
+        plt.plot(X[i][0], X[i][1], 'rx')
+
+
+def perceptron_sgd(X, Y):
+    w = np.zeros(len(X[0]))
+    b = 0
+    learning_rate = 1
+    epochs = 9
+
+    for i in range(epochs):
+        for j, x in enumerate(X):
+            if ((np.dot(w, X[j]) + b) * Y[j]) <= 0:
+                w = w + learning_rate * X[j] * Y[j]
+                b = b + learning_rate * Y[j]
+                break
+        print w, b
+    return w, b
+
+
+perceptron_sgd(X, y)
